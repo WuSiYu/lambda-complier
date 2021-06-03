76f037cbdce7ec6108dc15708c3f26464db125df
SiYu Wu <siyu@isrc.iscas.ac.cn>
2021-06-03 15:48:43 +0800

    add lambda-pack

--- ast_execute.py
+++ ast_execute.py
@@ -1,3 +1,4 @@
+from functools import partial
 import parser
 from scanner import get_scanner
 from parser import get_parser, parse, dump_ast
@@ -24,15 +25,15 @@ class ThreeAddressCodeEnv:
         self._last_flag += 1
         return 'label%d' % self._last_flag
 
-    def newtemp(self, type):
+    def newtemp(self, type_):
         self._last_mem += 1
-        return '#%s%d' % (type, self._last_mem)
+        return '#%s%d' % (type_, self._last_mem)
 
-    def newvar(self, idn, type):
+    def newvar(self, idn, type_):
         var = self.newnode()
         self._last_mem += 1
-        var.type = type
-        var.place = '#%s%d_%s' % (type, self._last_mem, idn)
+        var.type = type_
+        var.place = '#%s%d_%s' % (type_, self._last_mem, idn)
         self._vars[idn] = var
         return var
 
@@ -85,6 +86,25 @@ class ThreeAddressCodeEnv:
         return n1 + ':\n'
 
 
+class InterpreterEnv_Var():
+    def __init__(self, type_):
+        self.val = None
+        self.type = type_
+        self.place = self
+
+    def __repr__(self):
+        return repr(self.val)
+
+class InterpreterEnv_Obj():
+    def __getattr__(self, name):
+        return self.__dict__[name]
+
+    def __setattr__(self, name, value):
+        self.__dict__[name] = value
+
+    def __repr__(self):
+        return self.__dict__.__repr__()
+
 class InterpreterEnv:
     def __init__(self):
         self._exec = []
@@ -94,37 +114,20 @@ class InterpreterEnv:
         self._pc = 0
 
     def newnode(self):
-        class Obj():
-            def __getattr__(self, name):
-                return self.__dict__[name]
-
-            def __setattr__(self, name, value):
-                self.__dict__[name] = value
-
-            def __repr__(self):
-                return self.__dict__.__repr__()
-        return Obj()
+        return InterpreterEnv_Obj()
 
     def newlabel(self):
         l = object()
         self._labels[l] = None
         return l
 
-    def newtemp(self, type):
-        class Var():
-            def __init__(self):
-                self.val = None
-                self.type = type
-                self.place = self
-
-            def __repr__(self):
-                return repr(self.val)
-        var = Var()
+    def newtemp(self, type_):
+        var = InterpreterEnv_Var(type_)
         self._mem.append(var)
         return var
 
-    def newvar(self, idn, type):
-        var = self.newtemp(type)
+    def newvar(self, idn, type_):
+        var = self.newtemp(type_)
         self._vars[idn] = var
         return var
 
@@ -140,51 +143,51 @@ class InterpreterEnv:
     def gen_nothing(self):
         return tuple()
 
-    def gen_assign(self, n1, n2):
-        def f():
-            n1.val = self._get(n2)
-        return (f, )
+    def _assign(self, n1, n2):
+        n1.val = self._get(n2)
+    def gen_assign(self, *args):
+        return (partial(self._assign, *args), )
 
-    def gen_conv(self, n1, n2, n3, n4):
-        def f():
-            if n2 == 'int':
-                n1.val = int(self._get(n3))
-            if n2 == 'float':
-                n1.val = float(self._get(n3))
-            else:
-                raise NotImplementedError("gen_conv(): int <-> float only, not %s -> %s" % (n4, n2))
-        return (f, )
+    def _conv(self, n1, n2, n3, n4):
+        if n2 == 'int':
+            n1.val = int(self._get(n3))
+        if n2 == 'float':
+            n1.val = float(self._get(n3))
+        else:
+            raise NotImplementedError("gen_conv(): int <-> float only, not %s -> %s" % (n4, n2))
+    def gen_conv(self, *args):
+        return (partial(self._conv, *args), )
 
-    def gen_plus(self, n1, n2, n3):
-        def f():
-            n1.val = self._get(n2) + self._get(n3)
-        return (f, )
+    def _plus(self, n1, n2, n3):
+        n1.val = self._get(n2) + self._get(n3)
+    def gen_plus(self, *args):
+        return (partial(self._plus, *args), )
 
     gen_fplus = gen_plus
 
-    def gen_minus(self, n1, n2, n3):
-        def f():
-            n1.val = self._get(n2) - self._get(n3)
-        return (f, )
+    def _minus(self, n1, n2, n3):
+        n1.val = self._get(n2) - self._get(n3)
+    def gen_minus(self, *args):
+        return (partial(self._minus, *args), )
 
     gen_fminus = gen_minus
 
-    def gen_multiply(self, n1, n2, n3):
-        def f():
-            n1.val = self._get(n2) * self._get(n3)
-        return (f, )
+    def _multiply(self, n1, n2, n3):
+        n1.val = self._get(n2) * self._get(n3)
+    def gen_multiply(self, *args):
+        return (partial(self._multiply, *args), )
 
     gen_fmultiply = gen_multiply
 
-    def gen_divide(self, n1, n2, n3):
-        def f():
-            n1.val = self._get(n2) // self._get(n3)
-        return (f, )
+    def _divide(self, n1, n2, n3):
+        n1.val = self._get(n2) // self._get(n3)
+    def gen_divide(self, *args):
+        return (partial(self._divide, *args), )
 
-    def gen_fdivide(self, n1, n2, n3):
-        def f():
-            n1.val = self._get(n2) / self._get(n3)
-        return (f, )
+    def _fdivide(self, n1, n2, n3):
+        n1.val = self._get(n2) / self._get(n3)
+    def gen_fdivide(self, *args):
+        return (partial(self._fdivide, *args), )
 
     def _cmp(self, n1, n2, n3):
         if n2 == '>':
@@ -194,17 +197,18 @@ class InterpreterEnv:
         if n2 == '<':
             return self._get(n1) < self._get(n3)
 
-    def gen_if(self, n1, n2, n3, n4):
-        def f():
-            if self._cmp(n1, n2, n3):
-                self._pc = self._labels[n4] - 1
-        return (f, )
+    def _if(self, n1, n2, n3, n4):
+        if self._cmp(n1, n2, n3):
+            self._pc = self._labels[n4] - 1
+    def gen_if(self, *args):
+        return (partial(self._if, *args), )
+
     gen_fif = gen_if
 
-    def gen_goto(self, n1):
-        def f():
-            self._pc = self._labels[n1] - 1
-        return (f, )
+    def _goto(self, n1):
+        self._pc = self._labels[n1] - 1
+    def gen_goto(self, *args):
+        return (partial(self._goto, *args), )
 
     def gen_label(self, n1):
         return (n1, )
